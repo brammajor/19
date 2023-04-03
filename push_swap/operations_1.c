f86b6b9df662daf451b0332cb994058e20f310ae
brammajor <majorbram@hotmail.com>
2023-04-03 11:43:08 +0200

    auto push

---
+++ push_swap/operations_1.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_1.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brmajor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 13:32:55 by brmajor           #+#    #+#             */
+/*   Updated: 2023/03/29 15:05:05 by brmajor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap(t_node **head, char x)
+{
+	t_node	*newhead;
+	t_node	*node2;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return ;
+	node2 = *head;
+	newhead = node2->next;
+	node2->next = newhead->next;
+	newhead->next = node2;
+	*head = newhead;
+	if (x == 's')
+		return ;
+	ft_printf("s%c\n", x);
+}
+
+void	pa(t_node **astack, t_node **bstack)
+{
+	t_node	*newhead;
+
+	if (*bstack == NULL)
+		return ;
+	newhead = *bstack;
+	*bstack = newhead->next;
+	newhead->next = *astack;
+	*astack = newhead;
+	ft_printf("pa\n");
+}
+
+void	pb(t_node **astack, t_node **bstack)
+{
+	t_node	*newhead;
+
+	if (*astack == NULL)
+		return ;
+	newhead = *astack;
+	*astack = newhead->next;
+	newhead->next = *bstack;
+	*bstack = newhead;
+	ft_printf("pb\n");
+}
+
+void	rotate(t_node **head, char x)
+{
+	t_node	*last;
+
+	last = *head;
+	while (head->next != NULL)
+		*head = (*head)->next;
+	last->next = NULL;
+	(*head)->next = last;
+	if (x = 'r')
+		return ;
+	ft_printf("p%c\n", x);
+}
--- push_swap/operations_2.c
+++ push_swap/operations_2.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   operations_2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brmajor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/24 13:39:06 by brmajor           #+#    #+#             */
-/*   Updated: 2023/03/29 14:14:20 by brmajor          ###   ########.fr       */
+/*   Created: 2023/03/29 16:02:55 by brmajor           #+#    #+#             */
+/*   Updated: 2023/03/29 16:03:55 by brmajor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft_custom/libft.h"
-# include "libft_custom/ft_printf.h"
-
-typedef struct	s_node
-{
-	int		content;
-	struct s_node	*next;
-}			t_node;
-
-int	check_dup(int ac, char **av);
-int	check_int(int ac, char **av);
-void	check_error(int ac, char **av);
-
-#endif
+void	rra(t_node **asta
